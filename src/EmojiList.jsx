db87d5bda4be038ee4b4a3b54931b29f6a4fcd43
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-01-31 12:04:44 +0100

    fetch emoji list from API

--- src/EmojiList.jsx
+++ src/EmojiList.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react"
+
 export default function EmojiList() {
+    const [emojis, setEmojis] = useState([]);
+    const API_KEY = "14d0ee6eebe21bf0c2799ab1e12109e6387b97d7";
+
+    useEffect(() => {
+        fetch("https://emoji-api.com/emojis?access_key=" + API_KEY)
+            .then((response) => response.json())
+            .then((data) => setEmojis(data))
+    }, []);
     return (
         <div>
         This is the EmojiList component.
         Je fais des tests.
+        <ul>
+            {emojis.map((emoji) => (
+            <li key={emoji.slug}>
+                <img src={emoji.url} alt={emoji.unicodeName} />
+                {emoji.character}
+            </li>
+            ))}
+        </ul>
         </div>
     )
 };
