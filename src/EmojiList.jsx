335a74b8a87b039d205d8c027b8f117e48ff5f4f
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2025-02-04 09:49:16 +0100

    useEffect refacto

--- src/EmojiList.jsx
+++ src/EmojiList.jsx
@@ -2,14 +2,41 @@ import { useState, useEffect } from "react"
 
 export default function EmojiList() {
     const [emojis, setEmojis] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     const API_KEY = "14d0ee6eebe21bf0c2799ab1e12109e6387b97d7";
 
     useEffect(() => {
-        fetch(`https://emoji-api.com/emojis?access_key=${API_KEY}`)
-            .then((response) => response.json())
-            .then((data) => setEmojis(data))
-            .catch((error) => console.error("Error while processing emojis list:", error));
-    }, []);
+        let ignore = false;
+        
+        const fetchEmoji = async () => {
+            const url = `https://emoji-api.com/emojis?access_key=${API_KEY}`
+            setIsLoading(true);
+
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+
+                if (ignore === false) {
+                    setEmojis(data);
+                    setError(null);
+                    setIsLoading(false);
+                }
+            } catch (error) {
+                if (ignore === false) {
+                    setEmojis(null);
+                    setError(error);
+                    setIsLoading(false);
+            }
+        } 
+    }
+    fetchEmoji();
+
+    return () => {
+        ignore = true
+    }
+}, []);
+
     return (
         <div>
         This is the EmojiList component.
